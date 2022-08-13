c0542f2af80dad38a725f6671e9f8736840ed254
0x218 <sa.renjith@gmail.com>
2022-08-13 18:15:10 -0500

    refactor, comments

--- test_driven/Rent-a-Car/Car_Rental.py
+++ test_driven/Rent-a-Car/Car_Rental.py
@@ -1,33 +1,47 @@
 from dataclasses import dataclass
-
-
-@dataclass
-# class Car_Renterer:
-#     def __init__(self, name_of_renter): 
-#         self.name = name_of_renter
-        
-#     def set_renterer_details(self, name):
-#         self.name = name
-    
-#     def get_renterer_details(self):
-#         return self.name
-
     
 @dataclass
 class CarRental:
-    def __init__(self, company_name):
-        self.company_name = company_name
-        self.premium_car_daily_rate = 100
-        self.non_premium_car_daily_rate = 40.0
-        self.insurance_per_day = 30
-        self.netCost = 0.0
-    
+    # def __init__(self, company_name):
+    #     self.company_name = company_name
+    #     self.premium_car_daily_rate = 100
+    #     self.non_premium_car_daily_rate = 40.0
+    #     self.insurance_per_day = 30
+    #     self.net_cost = 0.0
+    #
+    #
+    # # magic function to return class object
+    # def __repr__(self):
+    #     return ("CarRental (company_name={}, premium_car_daily_rate={}, non_premium_car_daily_rate={}, insurance_per_day={},netCost={} )"
+    #             .format(self.company_name, self.premium_car_daily_rate, self.non_premium_car_daily_rate, self.insurance_per_day,self.net_cost,))
+    #
+    #
+    # # magic function to return boolean
+    # def __eq__(self, cmpObj):
+    #     return ((self.company_name, self.premium_car_daily_rate, self.non_premium_car_daily_rate, self.insurance_per_day, self.net_cost,) ==
+    #          (cmpObj.company_name, cmpObj.premium_car_daily_rate, cmpObj.non_premium_car_daily_rate, cmpObj.insurance_per_day,cmpObj.net_cost))
+    #
+    #
+
+
+    ## dataclasses automatially includes __init__(), __repr__() and __eq__().
+    ## No need for the __init__, but uninitialized variables become mandatory arguments (here it is company_name).
+
+      
+    company_name: str ##uninitialized variables should be declared first
+    premium_car_daily_rate : float = 100
+    non_premium_car_daily_rate : float = 40.0
+    insurance_per_day : int = 30
+    net_cost : float = 0.0
+
     def Rent_a_car(self, number_of_days, premium_car):
         if premium_car:
-            self.netCost = (self.premium_car_daily_rate + self.insurance_per_day) * number_of_days
+            self.net_cost = (self.premium_car_daily_rate + self.insurance_per_day) * number_of_days
         else:
-             self.netCost = (self.non_premium_car_daily_rate + self.insurance_per_day) * number_of_days
+             self.net_cost = (self.non_premium_car_daily_rate + self.insurance_per_day) * number_of_days
         
     def Rental_cost(self) -> float:
-        return self.netCost
+        return self.net_cost
+
+
 
--- test_driven/Rent-a-Car/test_CarRental.py
+++ test_driven/Rent-a-Car/test_CarRental.py
@@ -1,10 +1,19 @@
+# 5 steps in TDD.
+## 1. write test to pass the feature specification.
+## 2. run the test and it will fail - as the feature is not implemented yet.
+## 3. write simplest code to make the test pass.
+## 4. modify core logic and ensure all tests pass
+## 5. Refactor and improvise code.
+### The process of writing failing test first, then write code that makes those test pass, and refactoring is called RED-GREEN-REFACTOR.
+
+
 import unittest
 from Car_Rental import CarRental
 
 
 class TestCarRental(unittest.TestCase):
     def setUp(self):
-        self.cr = CarRental("Enterprise Cars")
+        self.cr = CarRental("Enterprise Cars")##carrental has dataclass decorator and thus uninitialzed variable (company_name) becomes mandatory parameter
     
     def test_Rent_a_car_cost_Premimum(self):
         self.cr.Rent_a_car(5, True)
